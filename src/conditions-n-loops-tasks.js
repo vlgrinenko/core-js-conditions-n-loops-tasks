bf371c933c268f327eb5451f61318945eeda7d00
vladimir.grinenko <S1vjycnhbr>
2024-01-02 14:01:54 +0300

    feat: add isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -261,8 +261,20 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
+
+  while (end > start) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+
+    start += 1;
+    end -= 1;
+  }
+
+  return true;
 }
 
 /**
